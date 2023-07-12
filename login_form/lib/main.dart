610710ba2ca8a90f531c5d847307575ef02e2348
AroojAslam <101873027+AroojAslam@users.noreply.github.com>
2023-07-12 18:37:11 +0500

    login ui

---
+++ login_form/lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const login());
+}
+
+class login extends StatelessWidget {
+  const login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(height: 50,),
+              Container(
+                height: 150,
+                child: const Image(image: AssetImage('assets/images/login.png')),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+               Padding(padding: EdgeInsets.all(10),
+                 child:  Column(
+                   children: [
+                     TextFormField(
+                       decoration: InputDecoration(
+                           hintText: 'Email',
+                           fillColor: Colors.blue.shade50,
+                           filled: true,
+                           prefixIcon: Icon(Icons.email,color:Colors.blue.shade200),
+                           focusedBorder: OutlineInputBorder(
+                             borderSide: BorderSide(color: Colors.blue.shade100),
+                             borderRadius: BorderRadius.circular(20),
+                           ),
+                           enabledBorder: OutlineInputBorder(
+                             borderSide: BorderSide(color: Colors.blue.shade50),
+                             borderRadius: BorderRadius.circular(20),
+                           )
+                       ),
+                     ),
+                   ],
+                 ),
+               ),
+              Padding(padding: EdgeInsets.all(10),
+                child:  Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Email',
+                          fillColor: Colors.blue.shade50,
+                          filled: true,
+                          prefixIcon: Icon(Icons.password,color:Colors.blue.shade200),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blue.shade100),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blue.shade50),
+                            borderRadius: BorderRadius.circular(20),
+                          )
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            Padding(padding: EdgeInsets.all(15),
+            child:  Align(alignment: Alignment.centerRight,
+              child:  Text('Forgot Password?' ,style: TextStyle(color: Colors.indigoAccent,fontSize: 15),),
+            ),
+            ),
+              Padding(padding:const EdgeInsets.all(10),
+              child:Container(
+                height: 50,
+                width: 400,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade300,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Center(
+                  child: Text('Login'),
+                ),
+              ),
+              ),
+              Padding(padding: EdgeInsets.all(10),
+              child: const Text('---------- OR ----------',style: TextStyle(color: Colors.grey,fontSize: 15),),),
+              Padding(padding:const EdgeInsets.all(10),
+                child:Container(
+                  height: 50,
+                  width: 400,
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade50,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child:const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(image: AssetImage('assets/images/google.png'),height:40,width: 40,),
+                     Center(
+                       child:  Text('Login With Google'),
+                     ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('New to Logistics?',style: TextStyle(color: Colors.grey,fontSize: 15),
+                    ),
+                    Text('Register' ,style: TextStyle(color: Colors.indigoAccent,fontSize: 15),),
+                  ],
+                )
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
